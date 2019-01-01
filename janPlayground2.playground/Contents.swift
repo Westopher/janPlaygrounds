8243ec2065c396c18142f2dbfd3d27610d9c42cb
West Kraemer <kraemerwest@gmail.com>
2019-01-01 11:29:10 -0500

    go back to arrays to finish problems

--- janPlayground2.playground/Contents.swift
+++ janPlayground2.playground/Contents.swift
@@ -10,3 +10,24 @@ func lastDigit(_ number: Int) -> Int {
 lastDigit(5235433634)
 
 
+func first(_ N: Int) -> [Int] {
+    var numbers:[Int] = []
+    for number in 1...N {
+        numbers.append(number)
+    }
+    return numbers
+}
+
+var listOfNumbers = [1, 2, 3, 10, 100]
+
+var sum = 0
+
+for i in listOfNumbers {
+    sum += i
+}
+
+print(sum)
+
+
+
+
