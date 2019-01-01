bad7b2794f3b1d88890d8b977ced2521b5689b88
West Kraemer <kraemerwest@gmail.com>
2019-01-01 11:00:28 -0500

    first function

---
+++ janPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+func min2(_ a: Int, _ b: Int) -> Int {
+    
+    if a > b { return a }
+        
+    else { if b > a return b }
+    
+}
+
+//min2(50, 10)
