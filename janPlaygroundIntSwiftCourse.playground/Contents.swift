22ec9ec7a8ac971e3e1f97a897027e0fb75c37a0
West Kraemer <kraemerwest@gmail.com>
2019-01-05 06:08:45 -0500

    implit unwrapping of objects

--- janPlaygroundIntSwiftCourse.playground/Contents.swift
+++ janPlaygroundIntSwiftCourse.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 import Foundation
 
 
-//optionals: implicit unwrapping
+//optionals: implicit unwrapping (optional binding)
 
 
 let imageURL: String? = "www...."
@@ -12,3 +12,41 @@ if let successfulURL = imageURL {
 } else {
     print("no image")
 }
+
+class Human {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func sayHello() {
+        print("Hello, I'm \(name).")
+    }
+}
+
+var West = Human(name: "West")
+West.sayHello()
+
+class Apartment {
+    var resident: Human? = nil
+}
+
+//Initialize
+var NYCApartment = Apartment()
+NYCApartment.resident = Human(name: "Westopher")
+
+
+NYCApartment.resident?.sayHello()
+let name = NYCApartment.resident?.name
+
+let ChicagoApartment = Apartment()
+ChicagoApartment.resident?.name
+ChicagoApartment.resident = Human(name: "Lip")
+
+if let residentName = ChicagoApartment.resident?.name {
+    print(residentName)
+    } else {
+    print("No name available")
+    }
+
