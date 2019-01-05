1e76ec2c9928e0fd7e4a73b62ed07bf2a8a201e9
West Kraemer <kraemerwest@gmail.com>
2019-01-05 05:40:05 -0500

    implicit unwrapping

--- janPlaygroundIntSwiftCourse.playground/Contents.swift
+++ janPlaygroundIntSwiftCourse.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
+import Foundation
 
-var str = "Hello, playground"
+
+//optionals: implicit unwrapping
+
+
+let imageURL: String? = "www...."
+
+if let successfulURL = imageURL {
+    print(successfulURL)
+} else {
+    print("no image")
+}
