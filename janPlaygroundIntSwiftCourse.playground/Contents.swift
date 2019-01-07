1fa69054ff9249044d4f69ddc8ed278121d79719
West Kraemer <kraemerwest@gmail.com>
2019-01-07 09:20:22 -0500

    add guard let to optionals

--- janPlaygroundIntSwiftCourse.playground/Contents.swift
+++ janPlaygroundIntSwiftCourse.playground/Contents.swift
@@ -30,7 +30,9 @@ West.sayHello()
 
 class Apartment {
     var resident: Human? = nil
-}
+
+    }
+
 
 //Initialize
 var NYCApartment = Apartment()
@@ -45,20 +47,39 @@ ChicagoApartment.resident?.name
 ChicagoApartment.resident = Human(name: "Lip")
 ChicagoApartment.resident?.sayHello()
 
+let sanFranApt = Apartment()
+sanFranApt.resident = Human(name: "Full House")
+sanFranApt.resident?.sayHello()
+
 if let residentName = ChicagoApartment.resident?.name {
     print(residentName)
     } else {
     print("No name available")
     }
 
+func checkResident() {
+    guard let residentName = sanFranApt.resident?.name else {
+        print("No name available")
+        return
+    }
+    print(residentName)
+    
+}
+checkResident()
+
+
+
+
 let iCanDrink = true
 
 func checkDrinkingAges() {
+
+    //if the condition is false, it is handled here. So you're "guarding" against the false. So if this value comes up as a nil or you don't get a value back from an API, you run code to handle that in the print line below. Then you exit the function. After that, you run code if you get something back.
     guard iCanDrink else {
         print("You can't drink")
         return
     }
-    
+    //it the condition is true, it is handled here
     print("Enjoy your night")
 }
 
