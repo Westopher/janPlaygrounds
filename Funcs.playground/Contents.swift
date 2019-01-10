4d4176695be45a6ea7b9ab524dbf95176434be89
West Kraemer <kraemerwest@gmail.com>
2019-01-10 16:00:04 -0500

    more funcs

--- Funcs.playground/Contents.swift
+++ Funcs.playground/Contents.swift
@@ -1,8 +1,21 @@
 import UIKit
 import Foundation
 
-func sayMyName(name: String) {
+func sayMyName(_ name: String) {
     print("\(name) is the name. Coding is my game.")
 }
 
-sayMyName(name: "West")
+sayMyName("West")
+
+
+let poundsOfCookies = 10
+let priceofCookies = 25
+
+func calcOrderPrice(pounds: Int, price: Int) -> Int {
+    let totalCost = pounds * price
+    return totalCost
+}
+
+let totalCost = calcOrderPrice(pounds: poundsOfCookies, price: priceofCookies)
+
+print("The total cost is \(totalCost)")
