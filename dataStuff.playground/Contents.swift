100abc7a3392424ff0fd757595aa8671d41da81f
West Kraemer <kraemerwest@gmail.com>
2019-01-14 11:46:36 -0500

    add encoding

--- dataStuff.playground/Contents.swift
+++ dataStuff.playground/Contents.swift
@@ -1,25 +1,44 @@
 import UIKit
 
-struct Customer : Decodable {
+
+/////decoding
+//struct Customer : Decodable {
+//
+//    var firstName :String
+//    var lastName :String
+//    var age :Int
+//}
+//
+//let json = """
+//
+//{
+//    "firstNae" : "John",
+//    "lastName" : "Doe",
+//    "age" : 34
+//}
+//
+//""".data(using: .utf8)!
+//
+//let customer = try? JSONDecoder().decode(Customer.self, from: json)
+//if let customer = customer {
+//print(customer)
+//} else {
+//    print("no json data")
+//}
+
+//encoding
+
+
+struct Customer : Codable {
     
-    var firstName :String
-    var lastName :String
-    var age :Int
+    var myName :String
+    var mylastName :String
+    var myAge :Int
 }
 
-let json = """
+let customer = Customer(myName: "West", mylastName: "Kraemer", myAge: 33)
+let encodedCustomerJSON = try! JSONEncoder().encode(customer)
 
-{
-    "firstNae" : "John",
-    "lastName" : "Doe",
-    "age" : 34
-}
+print(encodedCustomerJSON)
 
-""".data(using: .utf8)!
-
-let customer = try? JSONDecoder().decode(Customer.self, from: json)
-if let customer = customer {
-print(customer)
-} else {
-    print("no json data")
-}
+print(String(data: encodedCustomerJSON, encoding: .utf8)!)
