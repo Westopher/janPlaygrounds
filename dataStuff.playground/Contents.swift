1541a50ce3d334d64da4ad64ad492f814ced2120
West Kraemer <kraemerwest@gmail.com>
2019-01-14 13:36:22 -0500

    decoding a dictionary with a string as key and array as value

--- dataStuff.playground/Contents.swift
+++ dataStuff.playground/Contents.swift
@@ -1,88 +1,120 @@
 import UIKit
 
-
-/////decoding
-//struct Customer : Decodable {
 //
-//    var firstName :String
-//    var lastName :String
-//    var age :Int
+///////decoding
+////struct Customer : Decodable {
+////
+////    var firstName :String
+////    var lastName :String
+////    var age :Int
+////}
+////
+////let json = """
+////
+////{
+////    "firstNae" : "John",
+////    "lastName" : "Doe",
+////    "age" : 34
+////}
+////
+////""".data(using: .utf8)!
+////
+////let customer = try? JSONDecoder().decode(Customer.self, from: json)
+////if let customer = customer {
+////print(customer)
+////} else {
+////    print("no json data")
+////}
+//
+////encoding
+//
+//
+//struct Customer : Codable {
+//
+//    var myName :String
+//    var mylastName :String
+//    var myAge :Int
+//}
+//
+//let customer = Customer(myName: "West", mylastName: "Kraemer", myAge: 33)
+//let encodedCustomerJSON = try! JSONEncoder().encode(customer)
+//
+//print(encodedCustomerJSON)
+//
+//print(String(data: encodedCustomerJSON, encoding: .utf8)!)
+//
+//
+////getting array data
+//
+//struct Place: Decodable {
+//    var name: String
+//    var latitude: Double
+//    var longitude: Double
+//    var visited: Bool
 //}
 //
 //let json = """
 //
+//[
 //{
-//    "firstNae" : "John",
-//    "lastName" : "Doe",
-//    "age" : 34
+//"name" : "Costa Rica",
+//"latitude" : 23.45,
+//"longitude" : 45.23,
+//"visited" : true
+//},
+//{
+//"name" : "Puerto Rico",
+//"latitude" : 23,
+//"longitude" : 45,
+//"visited" : true
+//},
+//{
+//"name" : "Mexico City",
+//"latitude" : 23,
+//"longitude" : 45,
+//"visited" : true
+//},
+//{
+//"name" : "Iceland",
+//"latitude" : 23,
+//"longitude" : 45,
+//"visited" : false
 //}
+//]
 //
 //""".data(using: .utf8)!
 //
-//let customer = try? JSONDecoder().decode(Customer.self, from: json)
-//if let customer = customer {
-//print(customer)
-//} else {
-//    print("no json data")
-//}
+//let places = try! JSONDecoder().decode([Place].self, from: json)
+//print(places[3].name)
 
-//encoding
+//decoding a dictionary with a string as key and an array as value
 
-
-struct Customer : Codable {
-    
-    var myName :String
-    var mylastName :String
-    var myAge :Int
-}
-
-let customer = Customer(myName: "West", mylastName: "Kraemer", myAge: 33)
-let encodedCustomerJSON = try! JSONEncoder().encode(customer)
-
-print(encodedCustomerJSON)
-
-print(String(data: encodedCustomerJSON, encoding: .utf8)!)
-
-
-//getting array data
-
-struct Place: Decodable {
+struct ModelName: Decodable {
     var name: String
     var latitude: Double
     var longitude: Double
-    var visited: Bool
 }
 
-let json = """
+let jsonData = """
 
-[
 {
-"name" : "Costa Rica",
-"latitude" : 23.45,
-"longitude" : 45.23,
-"visited" : true
-},
-{
-"name" : "Puerto Rico",
-"latitude" : 23,
-"longitude" : 45,
-"visited" : true
-},
-{
-"name" : "Mexico City",
-"latitude" : 23,
-"longitude" : 45,
-"visited" : true
-},
-{
-"name" : "Iceland",
-"latitude" : 23,
-"longitude" : 45,
-"visited" : false
+     "places":[
+            {
+                "name" : "Costa Rica",
+                "latitude" : 34.56,
+                "longitude" : 65.67
+            },
+            {
+                "name" : "Boston",
+                "latitude" : 134.56,
+                "longitude" : 265.67
+            }
+            ]
 }
-]
+
 
 """.data(using: .utf8)!
 
-let places = try! JSONDecoder().decode([Place].self, from: json)
-print(places[3].name)
+let placesDictionary = try! JSONDecoder().decode([String:[ModelName]].self, from: jsonData)
+let placesJSON = placesDictionary["places"]
+print(placesJSON)
