0de0630b05591f0996e32eec3acf60087ba17599
West Kraemer <kraemerwest@gmail.com>
2019-01-14 12:15:44 -0500

    parsing arrays

--- dataStuff.playground/Contents.swift
+++ dataStuff.playground/Contents.swift
@@ -42,3 +42,47 @@ let encodedCustomerJSON = try! JSONEncoder().encode(customer)
 print(encodedCustomerJSON)
 
 print(String(data: encodedCustomerJSON, encoding: .utf8)!)
+
+
+//getting array data
+
+struct Place: Decodable {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+    var visited: Bool
+}
+
+let json = """
+
+[
+{
+"name" : "Costa Rica",
+"latitude" : 23.45,
+"longitude" : 45.23,
+"visited" : true
+},
+{
+"name" : "Puerto Rico",
+"latitude" : 23,
+"longitude" : 45,
+"visited" : true
+},
+{
+"name" : "Mexico City",
+"latitude" : 23,
+"longitude" : 45,
+"visited" : true
+},
+{
+"name" : "Iceland",
+"latitude" : 23,
+"longitude" : 45,
+"visited" : false
+}
+]
+
+""".data(using: .utf8)!
+
+let places = try! JSONDecoder().decode([Place].self, from: json)
+print(places[3].name)
