1f8edae917de98130d93f71a8b26f825847f6c78
West Kraemer <kraemerwest@gmail.com>
2019-01-15 18:44:20 -0500

    add more examples

--- moreJSON.playground/Contents.swift
+++ moreJSON.playground/Contents.swift
@@ -13,12 +13,19 @@ var anotherInt = "world"
 swapTwoValues(&someInt, &anotherInt)
 print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
 
+//generics examples
+let highSchoolGPA = [2.8, 3.0, 3.5, 3.8]
+let favoritePeople = ["Michael Jordan", "Lebron James", "Patrick Ewing", "Shaq", "Kobe"]
+let favoriteNumbers = [3, 5, 6, 7, 186]
 
+//so, you don't know what kind of array it's going to be. It could be double, string, or set
+func printElement<Generic>(array: [Generic]) {
+    for i in array {
+        print(i)
+    }
+}
 
-
-
-
-
+printElement(array: favoritePeople)
 
 
 
