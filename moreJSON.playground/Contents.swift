93b1c453c6db1292de7720e18be053967908f073
West Kraemer <kraemerwest@gmail.com>
2019-01-15 18:25:28 -0500

    add generic types

---
+++ moreJSON.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+
+var someInt = 3
+var anotherInt = 107
+swapTwoInts(&someInt, &anotherInt)
+print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
+
+
+
+
+
+
+
+
+
+
+
+
+
