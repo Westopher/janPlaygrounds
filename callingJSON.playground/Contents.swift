3939a3ad5b2bce643caa2fde6e33e1f299afd5e5
West Kraemer <kraemerwest@gmail.com>
2019-01-20 16:52:10 -0500

    add models

--- callingJSON.playground/Contents.swift
+++ callingJSON.playground/Contents.swift
@@ -1,6 +1,28 @@
 import UIKit
 import PlaygroundSupport
 
+struct Users {
+    let id: Int
+    let username: String
+    let email: String
+    let address: Address
+}
+
+struct Address {
+    let street: String
+    let suite: String
+    let city: String
+    let zipcode: String
+    let geo: Geo
+}
+
+struct Geo {
+    let lat: String
+    let lng: String
+}
+
+
+
 let url = URL(string: "https://jsonplaceholder.typicode.com/users")
 
 URLSession.shared.dataTask(with: url!) { data, response, error in
