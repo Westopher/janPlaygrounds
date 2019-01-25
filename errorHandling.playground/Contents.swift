f4c8032f631e447cd9fc3cd8eefea4912f8064b6
West Kraemer <kraemerwest@gmail.com>
2019-01-25 17:27:55 -0500

    do try catch error handling

--- errorHandling.playground/Contents.swift
+++ errorHandling.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
-var str = "Hello, playground"
+
+
+enum HeightError: Error {
+    case maxHeight
+    case minHeight
+}
+
+func checkHeightError(height: Int) throws {
+    if height > 200 {
+        throw HeightError.maxHeight
+    } else if height < 140 {
+        throw HeightError.minHeight
+    } else {
+        print("enjoy")
+    }
+}
+
+do {
+    try checkHeightError(height: 145)
+}   catch HeightError.maxHeight {
+    print("too tall")
+}   catch HeightError.minHeight {
+    print("too short")
+}
+
