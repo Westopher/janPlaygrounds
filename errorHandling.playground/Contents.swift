b038cc5b842609effe441d350f7232ae0ddeb21f
West Kraemer <kraemerwest@gmail.com>
2019-01-25 18:16:39 -0500

    more working with get set

--- errorHandling.playground/Contents.swift
+++ errorHandling.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Foundation
 
 
 
@@ -24,4 +25,55 @@ do {
 }   catch HeightError.minHeight {
     print("too short")
 }
+
+// get + set
+
+//var radius: Double = 10
+//
+//var diameter: Double {
+//    get {
+//        return radius * 2
+//    }
+//    set {
+//        radius = newValue / 2
+//    }
+//}
+//
+//diameter = 50
+
+//var side: Double = 100
+//
+//var area: Double {
+//    get {
+//        return side * side
+//    }
+//    set {
+//        side = sqrt(newValue)
+//    }
+//}
+//
+//side = 9
+
+struct fruitPrices {
+    var apple: Double
+    var orange: Double
+    var peach: Double
+    var banana: Double {
+        get {
+            return peach * 2
+        }
+        set {
+            apple = newValue + 1
+            orange = newValue + 2
+            peach = newValue + 3
+        }
+    }
+}
+
+var fruit = fruitPrices(apple: 10, orange: 20, peach: 50)
+
+fruit.apple
+fruit.orange
+fruit.peach
+fruit.banana
 
