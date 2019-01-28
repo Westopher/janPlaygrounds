85feb84130bfc384fa5546618e877cef9b14e018
West Kraemer <kraemerwest@gmail.com>
2019-01-28 17:17:56 -0500

    return closure directly inside function

--- closures.playground/Contents.swift
+++ closures.playground/Contents.swift
@@ -10,3 +10,22 @@ var addClosures$: (Int, Int, Int) -> Int = { $0 + $1 + $2 }
 addClosures$(5, 10, 20)
 addClosures$(100,200,300)
 
+var returnString: () -> String = { () in
+    "I am a string"
+}
+returnString
+func returnClosure() -> ((Int, Int) -> Int) {
+   return addClosures
+}
+ let returnedClosure = returnClosure()
+returnedClosure(10,10)
+
+//return a closure within a function
+
+func returnClosureInFunction() -> ((Int, Int) -> Int) {
+    return {
+        (number1, number2) in number1 + number2
+    }
+}
+let closure = returnClosureInFunction()
+closure(10,10)
