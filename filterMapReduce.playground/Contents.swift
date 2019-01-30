71671b3c0504b6e58905a740e283db6db3c4f531
West Kraemer <kraemerwest@gmail.com>
2019-01-30 17:32:49 -0500

    I don't like compound assignment operators.||| sum = sum + element||| is how I roll

---
+++ filterMapReduce.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+
+
+let numbers = [4,5,6]
+var sum = 0
+for number in numbers {
+    sum += number
+}
+print(sum)
+
+func funcToSaveTime(elements: [Int]) -> Int {
+    var sum = 0
+    for element in elements {
+       //compound assignment operators are unclear and I don't like them.
+        sum = sum + element
+    }
+    return sum
+}
+
+funcToSaveTime(elements: [15,15])
+
+
+
+
+
+
+
+
+
