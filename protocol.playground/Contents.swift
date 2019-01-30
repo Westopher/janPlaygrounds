ef6b7174c68cbdb47d500a70e4ab2745229d4fe6
West Kraemer <kraemerwest@gmail.com>
2019-01-30 17:06:33 -0500

    add protocols

--- protocol.playground/Contents.swift
+++ protocol.playground/Contents.swift
@@ -1,3 +1,32 @@
 import UIKit
 
-var str = "Hello, playground"
+
+protocol Humanable {
+    var name: String {get set}
+    var race: String {get set}
+    func sayHi()
+}
+
+struct Korean: Humanable {
+    var name: String = "Bob Lee"
+    var race: String = "Asian"
+    func sayHi() {
+        print("Hi there, I'm \(name)")
+    }
+}
+
+struct American: Humanable {
+    var name: String = "West K"
+    var race: String = "White"
+    func sayHi() {
+        print("Hi there, I'm \(name)")
+    }
+}
+
+
+
+
+
+
+
+
